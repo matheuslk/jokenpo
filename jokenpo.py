dfcdf000d14150f229677d3c8b2c5b13d100053a
matheuslk <matheus.augustolk@gmail.com>
2022-04-16 16:21:10 -0300

    finish game without testing

--- jokenpo.py
+++ jokenpo.py
@@ -12,108 +12,110 @@ while option != '4':
   print('Obs: Digite qualquer outra tecla para sair\n')
   option = input('\nEscolha uma opção: ')
   if option == '1' or option == '2' or option == '3':
-    exit_selected_mode = False
-    player_one_points = 0
-    player_two_points = 0
-    draws = 0
-    matches = 1
-    player_won = 0  # could be player one (1) or two (2) or anybody (0)
-    continue_playing = 0  # could be YES (1) or NO (0)
-    skip_score = 0
-    proceed_to_selected_mode = ''
-    player_one_option = ''
-    player_two_option = ''
-    player_one_result_option = ''
-    player_two_result_option = ''
-    if option == '2':
-      os.system('cls||clear')
+    os.system('cls||clear')
+    if option == '1':
+      print('Você escolheu Jogador vs Jogador, deseja continuar ?\n')
+      player_one_name = 'Jogador1'
+      player_two_name = 'Jogador2'
+      number_of_input_options = 2
+    elif option == '2':
       print('Você escolheu Jogador vs Computador, deseja continuar ?\n')
-      print('>(1) Sim\n')
-      print('Obs: Digite qualquer outra tecla para sair\n')
-      proceed_to_selected_mode = input('\nEscolha uma opção: ')
-      if proceed_to_selected_mode == '1':
-        while matches != 4 or not exit_selected_mode:
-          os.system('cls||clear')
-          print(
-              f'Jogador(Venceu {player_one_points}) vs Computador(Venceu {player_two_points})\n')
-          print(f'Total de Partidas: {matches - 1}\n\n')
-          print('>(1) Pedra\n')
-          print('>(2) Papel\n')
-          print('>(3) Tesoura\n')
-          print('Obs: Digite qualquer outra tecla para sair\n')
-          player_one_option = input('\nEscolha uma opção: ')
-          if(player_one_option == '1' or player_one_option == '2' or player_one_option == '3'):
-            player_two_option = str(randint(1, 3))
-            if(player_one_option == '1' and player_two_option == '2'):
-              player_won = 2
-            elif(player_one_option == '2' and player_two_option == '1'):
-              player_won = 1
-            elif(player_one_option == '3' and player_two_option == '1'):
-              player_won = 2
-            elif(player_two_option == '3' and player_one_option == '1'):
-              player_won = 1
-            elif(player_one_option == '3' and player_two_option == '2'):
-              player_won = 1
-            elif(player_two_option == '3' and player_one_option == '2'):
-              player_won = 2
-            elif(player_one_option == player_two_option):
-              player_won = 0
-            if player_won == 1:
-              player_one_points += 1
-            elif player_won == 2:
-              player_two_points += 1
-
-            if(player_one_option == 1):
-              player_one_result_option = 'Pedra'
-            elif(player_one_option == 2):
-              player_one_result_option = 'Papel'
-            else:
-              player_one_result_option = 'Tesoura'
-
-            if(player_two_option == 1):
-              player_two_result_option = 'Pedra'
-            elif(player_two_option == 2):
-              player_two_result_option = 'Papel'
-            else:
-              player_two_result_option = 'Tesoura'
+      player_one_name = 'Jogador'
+      player_two_name = 'Computador'
+      number_of_input_options = 1
+    elif option == '3':
+      print('Você escolheu Computador vs Computador, deseja continuar ?\n')
+      player_one_name = 'Computador1'
+      player_two_name = 'Computador2'
+    print('>(1) Sim\n')
+    print('Obs: Digite qualquer outra tecla para sair\n')
+    proceed_to_selected_mode = input('\nEscolha uma opção: ')
+    if proceed_to_selected_mode == '1':
+      player_one_points = 0
+      player_two_points = 0
+      draws = 0
+      matches = 1
+      exit_selected_mode = False
+      while not exit_selected_mode:
+        if(option == '1' or option == '2'):
+          counter = 0
+          while counter < number_of_input_options:
             os.system('cls||clear')
-            print('Resumo da partida:\n')
-            print(f'Jogador escolheu: {player_one_result_option}\n')
-            print(f'Computador escolheu: {player_two_result_option}\n')
-            if(player_won == 1):
-              print('Jogador venceu !\n')
-            elif(player_won == 2):
-              print('Computador venceu !\n')
-            else:
-              draws += 1
-              print('Ocorreu um empate !\n')
-            print('\nDeseja continuar?\n')
-            print('>(1) Sim\n')
+            print('>(1) Pedra\n')
+            print('>(2) Papel\n')
+            print('>(3) Tesoura\n')
             print('Obs: Digite qualquer outra tecla para sair\n')
-            continue_playing = int(input('\nEscolha uma opção: '))
-
-            if(continue_playing != 1):
-              exit_selected_mode
-            matches += 1
-
-            if(matches == 4):
-              while(skip_score == 0):
-                os.system('cls||clear')
-                print(
-                    f'Total de partidas jogadas: {matches-1}\n')
-                print(
-                    f'Partidas vencidas pelo jogador 1: {player_one_points}\n')
-                print(
-                    f'Partidas vencidas pelo computador: {player_two_points}\n')
-                print(
-                    f'Número de empates: {draws}\n')
-                int(input('\n>Digite qualquer tecla para sair'))
-                skip_score = 1
+            if(option == '1' and counter == 0):
+              player_one_option = input(
+                  f'\nEscolha uma opção {player_one_name}: ')
+            elif(option == '1' and counter == 1):
+              player_two_option = input(
+                  f'\nEscolha uma opção {player_two_name}: ')
+            elif(option == '2'):
+              player_one_option = input('\nEscolha uma opção: ')
+              player_two_option = str(randint(1, 3))
+            else:
+              exit_selected_mode = True
+              counter = number_of_input_options
+            counter += 1
+        else:
+          player_one_option = str(randint(1, 3))
+          player_two_option = str(randint(1, 3))
+        if(player_one_option == '1' or player_one_option == '2' or player_one_option == '3' and player_two_option == '1' or player_two_option == '2' or player_two_option == '3'):
+          if(player_one_option == '1' and player_two_option == '3' or player_one_option == '2' and player_two_option == '1' or player_one_option == '3' and player_two_option == '2'):
+            player_won = 1
+            player_one_points += 1
+            winner_name = player_one_name
+          elif(player_two_option == '1' and player_one_option == '3' or player_two_option == '2' and player_one_option == '1' or player_two_option == '3' and player_one_option == '2'):
+            player_won = 2
+            player_two_points += 1
+            winner_name = player_two_name
+          elif(player_one_option == player_two_option):
+            player_won = 0
+            draws += 1
+          if(player_one_option == '1'):
+            player_one_result_option = 'Pedra'
+          elif(player_one_option == '2'):
+            player_one_result_option = 'Papel'
           else:
+            player_one_result_option = 'Tesoura'
+          if(player_two_option == '1'):
+            player_two_result_option = 'Pedra'
+          elif(player_two_option == '2'):
+            player_two_result_option = 'Papel'
+          else:
+            player_two_result_option = 'Tesoura'
+          os.system('cls||clear')
+          print('Resumo da partida:\n')
+          print(f'{player_one_name} escolheu: {player_one_result_option}\n')
+          print(f'{player_two_name} escolheu: {player_two_result_option}\n')
+          if(player_won != 0):
+            print(f'Resultado: {winner_name} venceu !\n')
+          else:
+            print('Ocorreu um empate !\n')
+          print('\nDeseja continuar?\n')
+          print('>(1) Sim\n')
+          print('Obs: Digite qualquer outra tecla para sair\n')
+          continue_playing = int(input('\nEscolha uma opção: '))
+          if(continue_playing != 1):
             exit_selected_mode = True
-  elif option == 4:
-    print('Até mais !')
+          matches += 1
+          if(matches == 4):
+            exit_selected_mode = True
+            win_percentage_player_one = (player_one_points/3)*100
+            win_percentage_player_two = (player_two_points/3)*100
+            os.system('cls||clear')
+            print(
+                f'Total de partidas jogadas: {matches-1}\n')
+            print(
+                f'Porcentagem de vitória {player_one_name}: {win_percentage_player_one:.2f}%\n')
+            print(
+                f'Porcentagem de vitória {player_two_name}: {win_percentage_player_two:.2f}%\n')
+            print(
+                f'Número de empates: {draws}\n')
+            input('\n>Digite qualquer tecla para voltar ao menu principal: ')
+        else:
+          exit_selected_mode = True
   else:
     os.system('cls||clear')
-    print(f"{error_message}")
-    option = 0
+    option = '4'
---
+++ teste.py
@@ -0,0 +1,4 @@
+points = 1
+matches = 4-1
+
+print(f'Percentage: {((points/matches)*100):.2f}')
